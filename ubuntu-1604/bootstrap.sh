95084e0e3409fe7803ebd3ef72b9887582ba598b
hyeonsangjeon <wingnut0310@gmail.com>
2019-06-05 16:38:16 +0900

    [hyeonsangjeon]ssh & adapt same rsa key

--- ubuntu-1604/bootstrap.sh
+++ ubuntu-1604/bootstrap.sh
@@ -19,6 +19,12 @@ else
     sudo echo "$USER:$USER" | chpasswd
 fi
 
+if [[ ! -z ROOT_PW ]]; then
+    sudo echo "root:$ROOT_PW" | chpasswd
+else
+    sudo echo "root:root" | chpasswd
+fi
+
 /usr/sbin/sshd -D
 
 CMD=${1:-"exit 0"}
