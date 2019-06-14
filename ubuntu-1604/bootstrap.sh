7be376a919f389f5a2ad7c6ca68d7d1603f53f32
modenaf360 <wingnut0310@gmail.com>
2019-06-15 02:56:04 +0900

    [hyeonsangjeon] ready to stg to hub

--- ubuntu-1604/bootstrap.sh
+++ ubuntu-1604/bootstrap.sh
@@ -3,33 +3,24 @@
 # Create a user in the start up if USER environment variable is given
 # EX: docker run  -e USER=groot  USER_PW=iamuser
 
-
- echo "COME LANE CHANGE"
 #CREATE USER
 if [ ! -z $USER ] && [  $USER != root ]; then
-    echo "COME 0000000"
     adduser --disabled-password --gecos ""  "$USER" > /dev/null
     usermod -aG sudo "$USER" > /dev/null
     echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
     if [[ ! -z $USER_PW ]]; then
-        echo "COME 44444"
         sudo echo "$USER:$USER_PW" | chpasswd
     else
-        echo "COME555"
         sudo echo "$USER:$USER" | chpasswd
     fi
 fi
 
 
 #MODIFY PASSWD
-
-
 if [[ ! -z $ROOT_PW ]]; then
     sudo echo "root:$ROOT_PW" | chpasswd
-    echo "COME 1111111"
 else
     sudo echo "root:root" | chpasswd
-    echo "COME 2222222"
 fi
 
 /usr/sbin/sshd -D
