8d10b4a85849850ecd6367307f35d2f09b2a4cca
modenaf360 <wingnut0310@gmail.com>
2019-06-29 01:36:53 +0900

    [hyeonsangjeon] mr_test script add

--- hadoop-3.2/example/test/mr_test.sh
+++ hadoop-3.2/example/test/mr_test.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # create dfs dir
 hdfs dfs -mkdir -p input
 
+sleep 2
 # put into data
 hdfs dfs -put ./simple-word.txt input
 
@@ -10,8 +11,11 @@ hdfs dfs -put ./simple-word.txt input
 echo -e "\simple-word.txt"
 hdfs dfs -cat input/simple-word.txt
 
+sleep 2
+
 hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/sources/hadoop-mapreduce-examples-3.2.0-sources.jar  org.apache.hadoop.examples.WordCount input output
 
+sleep 2
 
 # check output of wordcount
 echo -e "\nresult:"
