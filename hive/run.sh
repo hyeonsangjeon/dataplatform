0652a3412f758a2a3f6d99657f8d3088fba8ea5a
hyeonsangjeon <wingnut0310@gmail.com>
2019-07-01 17:46:08 +0900

    [hyeonsangjeon] When the initial container is started, it judges whether there is a hive log and decides to create a table schema. DB type flag added, standalone / external metastore decision

--- hive/run.sh
+++ hive/run.sh
@@ -1,13 +1,45 @@
 #!/bin/bash
 
-hdfs dfs -mkdir       /tmp
-hdfs dfs -mkdir -p    /user/hive/warehouse
-hdfs dfs -chmod g+w   /tmp
-hdfs dfs -chmod g+w   /user/hive/warehouse
+service ssh start
 
+
+##################################################
+# dbtype : mysql [external metastore db]
+# dbtype : derby [standalone internal metastore db]
+##################################################
+dbtype=$DB_TYPE
+
+initdir=`echo /log_data/hive | perl -pe 's#file://##'`
+
+
+if [ "`ls -A $initdir --ignore='lost+found'`" == "" ]; then
+    echo "##################################################"
+    echo "Making initial hive hdfs directory!!!"
+    echo "##################################################"
+    hdfs dfs -mkdir       /tmp
+    hdfs dfs -mkdir -p    /user/hive/warehouse
+    hdfs dfs -chmod g+w   /tmp
+    hdfs dfs -chmod g+w   /user/hive/warehouse
+
+    echo "##################################################"
+    echo "Create initial schema to metastore db: $dbtype"
+    echo "##################################################"
+    schematool -initSchema -dbType $dbtype
+fi
+
+
+sleep 2
+
+echo "##################################################"
+echo " Starting metastore conection"
+echo "##################################################"
 hive --service metastore &
 
 sleep 10
+
+echo "##################################################"
+echo " Starting hiveserver2 "
+echo "##################################################"
 hive --service hiveserver2 &
 
 
